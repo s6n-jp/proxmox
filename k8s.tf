e54b177bee2cd1dfbee4c3b77e58e46ba5f1dcb2
Natsuki Ikeguchi <me@s6n.jp>
2024-07-14 15:37:21 +0900

    fix: Configure OS type and boot order

--- k8s.tf
+++ k8s.tf
@@ -9,7 +9,9 @@ resource "proxmox_vm_qemu" "k8s_control" {
   cores                  = 2
   memory                 = 4096
   scsihw                 = "virtio-scsi-single"
+  os_type                = "l26"
   bios                   = "ovmf"
+  boot                   = "order=scsi0;ide2"
   agent                  = 1
   onboot                 = true
   full_clone             = false
@@ -55,7 +57,9 @@ resource "proxmox_vm_qemu" "k8s_worker" {
   cores                  = 8
   memory                 = 16384
   scsihw                 = "virtio-scsi-single"
+  os_type                = "l26"
   bios                   = "ovmf"
+  boot                   = "order=scsi0;ide2"
   agent                  = 1
   onboot                 = true
   full_clone             = false
